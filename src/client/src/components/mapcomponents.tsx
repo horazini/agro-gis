57ea5b4ccf18df8c9d29bdb98b92633e3e695db8
Horacio Zini <horazini@gmail.com>
2023-06-17 20:45:36 -0300

    Refactoring: unique "map components" file for frequent use.

---
+++ src/client/src/components/mapcomponents.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import {
+  LayersControl,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { LatLngExpression } from "leaflet";
+
+import L from "leaflet";
+
+export const position: LatLngExpression = [-29, -58];
+
+const LeafIcon: any = L.Icon.extend({
+  options: {
+    iconSize: [25, 40],
+    shadowSize: [50, 64],
+    iconAnchor: [12.5, 40],
+    shadowAnchor: [4, 62],
+    popupAnchor: [-3, -50],
+  },
+});
+
+const icon = new LeafIcon({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+});
+
+// Ubicacion actual del usuario
+function LocationMarker() {
+  const [userPosition, setUserPosition] = useState<LatLngExpression>([0, 0]);
+
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setUserPosition(e.latlng);
+      //map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+  return userPosition === null ? null : (
+    <Marker position={userPosition} icon={icon}>
+      <Popup>Posición actual</Popup>
+    </Marker>
+  );
+}
+
+export function LayerControler(): JSX.Element {
+  return (
+    <LayersControl position="topright">
+      <LayersControl.BaseLayer checked name="Esri.WorldImagery">
+        <TileLayer
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+        />
+      </LayersControl.BaseLayer>
+      <LayersControl.BaseLayer name="Open Topo Map">
+        <TileLayer
+          url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+          attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>) &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </LayersControl.BaseLayer>
+      <LayersControl.BaseLayer name="Open Street Map">
+        <TileLayer
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </LayersControl.BaseLayer>
+
+      <LayersControl.Overlay name="Posición actual">
+        <LocationMarker />
+      </LayersControl.Overlay>
+    </LayersControl>
+  );
+}
--- src/client/src/layouts/maps/map3.tsx
+++ src/client/src/layouts/maps/map3.tsx
@@ -2,20 +2,15 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 import { useEffect, useState } from "react";
 import {
-  LayersControl,
   MapContainer,
-  Marker,
   Popup,
-  TileLayer,
-  useMapEvents,
   Circle,
   LayerGroup,
   Polygon,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { LatLngExpression, LayerEvent } from "leaflet";
+import { LayerEvent } from "leaflet";
 
-import L from "leaflet";
 import { RootState } from "../../redux/store";
 import { useSelector } from "react-redux";
 
@@ -28,75 +23,13 @@ import {
 import { ConfirmButton } from "../../components/confirmform";
 import { Feature } from "geojson";
 
+import { position, LayerControler } from "../../components/mapcomponents";
+
 interface ICrop {
   landplot: number;
   species: number;
   tenant_id: number;
   date: string;
-}
-
-const position: LatLngExpression = [-29, -58];
-
-const LeafIcon: any = L.Icon.extend({
-  options: {
-    iconSize: [25, 40],
-    shadowSize: [50, 64],
-    iconAnchor: [12.5, 40],
-    shadowAnchor: [4, 62],
-    popupAnchor: [-3, -50],
-  },
-});
-var icon = new LeafIcon({
-  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
-});
-
-// Ubicacion actual del usuario
-function LocationMarker() {
-  const [userPosition, setUserPosition] = useState<LatLngExpression>([0, 0]);
-
-  const map = useMapEvents({
-    click() {
-      map.locate();
-    },
-    locationfound(e) {
-      setUserPosition(e.latlng);
-      //map.flyTo(e.latlng, map.getZoom());
-    },
-  });
-  return userPosition === null ? null : (
-    <Marker position={userPosition} icon={icon}>
-      <Popup>Posición actual</Popup>
-    </Marker>
-  );
-}
-
-function LayerControler(): JSX.Element {
-  return (
-    <LayersControl position="topright">
-      <LayersControl.BaseLayer checked name="Esri.WorldImagery">
-        <TileLayer
-          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-          attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-        />
-      </LayersControl.BaseLayer>
-      <LayersControl.BaseLayer name="Open Topo Map">
-        <TileLayer
-          url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-          attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>) &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </LayersControl.BaseLayer>
-      <LayersControl.BaseLayer name="Open Street Map">
-        <TileLayer
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </LayersControl.BaseLayer>
-
-      <LayersControl.Overlay name="Posición actual">
-        <LocationMarker />
-      </LayersControl.Overlay>
-    </LayersControl>
-  );
 }
 
 const MapView = () => {
@@ -227,14 +160,11 @@ const MapView = () => {
       feature.geometry.type === "Point" &&
       feature.properties.subType === "Circle"
     ) {
-      const [lat, lng] = feature.geometry.coordinates;
-      const radius = feature.properties.radius;
-
       return (
         <Circle
           key={feature.properties.id}
-          center={[lat, lng]}
-          radius={radius}
+          center={feature.geometry.coordinates}
+          radius={feature.properties.radius}
           pathOptions={pathOptions}
           eventHandlers={eventHandlers}
         >

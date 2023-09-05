82b358b4ea39c3b10ae785796da4deb0a6ca58db
Horacio Zini <horazini@gmail.com>
2023-09-05 19:04:01 -0300

    minor style changes

--- src/client/src/components/footer.tsx
+++ src/client/src/components/footer.tsx
@@ -1,41 +1,39 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary">
-      {'Copyright © '}
-      <Link color="inherit" href="http://localhost:3000/">
+      {"Copyright © "}
+      <Link color="inherit" href="/">
         Nombre aplicación
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 export default function StickyFooter() {
   return (
-      <Box
-        component="footer"
-        sx={{
-          py: 3,
-          px: 2,
-          mt: 'auto',
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
-              ? theme.palette.grey[200]
-              : theme.palette.grey[800],
-        }}
-      >
-        <Container maxWidth="sm">
-          <Typography variant="body1">
-            Texto de footer.
-          </Typography>
-          <Copyright />
-        </Container>
-      </Box>
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        px: 2,
+        mt: "auto",
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+      }}
+    >
+      <Container maxWidth="sm">
+        <Typography variant="body1">Texto de footer.</Typography>
+        <Copyright />
+      </Container>
+    </Box>
   );
 }
--- src/client/src/layouts/app/home.tsx
+++ src/client/src/layouts/app/home.tsx
@@ -1,15 +1,42 @@
+import { Container, Grid, Paper } from "@mui/material";
+
 const Home = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "25vh",
-      }}
-    >
-      <h1>Home</h1>
-    </div>
+    <Container maxWidth="xl">
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={8} lg={7}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          >
+            {/* <Chart /> */}
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={4} lg={5}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          >
+            {/* <Deposits /> */}
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper
+            sx={{ p: 2, display: "flex", flexDirection: "column", height: 240 }}
+          >
+            {/* <Orders /> */}
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 

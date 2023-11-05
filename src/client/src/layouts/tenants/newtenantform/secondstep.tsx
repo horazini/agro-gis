2b58dd0161e7a1723a32f315801b01b7ebf05b4a
Horacio Zini <horazini@gmail.com>
2023-11-05 15:51:41 -0300

    form style changes

--- src/client/src/layouts/tenants/newtenantform/secondstep.tsx
+++ src/client/src/layouts/tenants/newtenantform/secondstep.tsx
@@ -15,6 +15,7 @@ import {
   MenuItem,
   InputLabel,
   FormControl,
+  IconButton,
 } from "@mui/material";
 
 import { Edit as EditIcon, Delete as DeleteIcon } from "@mui/icons-material";
@@ -22,6 +23,7 @@ import React, { useState } from "react";
 import { usernameAlreadyExists } from "../../../utils/services";
 import { isValidEmail } from "../../../utils/functions";
 import { tenantUserTypes } from "../../../utils/functions";
+import { DialogComponent } from "../../../components/customComponents";
 
 export interface RowData {
   id: number;
@@ -68,9 +70,7 @@ const SecondStep = ({
     setEditingRowId(row.id);
   };
 
-  const handleSubmitForm = () => {
-    //event.preventDefault();
-    handleSubmitUser(userData, editingRowId);
+  const clearAllFields = () => {
     setUserData({
       id: 0,
       username: "",
@@ -82,9 +82,24 @@ const SecondStep = ({
     setEditingRowId(null);
   };
 
+  const handleSubmitForm = () => {
+    handleSubmitUser(userData, editingRowId);
+    clearAllFields();
+  };
+
+  const userDelete = (rowId: number) => {
+    handleDeleteUser(rowId);
+    clearAllFields();
+  };
+
   const disableSubmit = () => {
     const { usertype_id, surname, names, mail_address, username } = userData;
     return !(usertype_id && surname && names && mail_address && username);
+  };
+
+  const disableClean = () => {
+    const { usertype_id, surname, names, mail_address, username } = userData;
+    return !usertype_id && !surname && !names && !mail_address && !username;
   };
 
   const [emailError, setEmailError] = useState(false);
@@ -136,10 +151,9 @@ const SecondStep = ({
       <Typography variant="h6" gutterBottom>
         Usuarios de la organización
       </Typography>
-
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <TableContainer component={Paper}>
+          <TableContainer component={Paper} style={{ padding: 10 }}>
             <Table>
               <TableHead>
                 <TableRow>
@@ -148,114 +162,163 @@ const SecondStep = ({
                   <TableCell>Nombres</TableCell>
                   <TableCell>Correo electrónico</TableCell>
                   <TableCell>Nombre de usuario</TableCell>
-                  <TableCell>Opciones</TableCell>
+                  <TableCell align="center">Opciones</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {userList.map((row) => (
-                  <TableRow key={row.id}>
-                    <TableCell>
-                      {
-                        tenantUserTypes.find(
-                          (usertype: any) => usertype.id === row.usertype_id
-                        )?.name
-                      }
-                    </TableCell>
-                    <TableCell>{row.surname}</TableCell>
-                    <TableCell>{row.names}</TableCell>
-                    <TableCell>{row.mail_address}</TableCell>
-                    <TableCell>{row.username}</TableCell>
-                    <TableCell>
-                      <Button onClick={() => handleEditRow(row)}>
-                        <EditIcon sx={{ mr: 1 }} />
-                      </Button>
-                      <Button onClick={() => handleDeleteUser(row.id)}>
-                        <DeleteIcon sx={{ mr: 1 }} />
-                      </Button>
+                {userList.length > 0 ? (
+                  userList.map((row) => (
+                    <TableRow key={row.id} selected={row.id === editingRowId}>
+                      <TableCell>
+                        {
+                          tenantUserTypes.find(
+                            (usertype: any) => usertype.id === row.usertype_id
+                          )?.name
+                        }
+                      </TableCell>
+                      <TableCell>{row.surname}</TableCell>
+                      <TableCell>{row.names}</TableCell>
+                      <TableCell>{row.mail_address}</TableCell>
+                      <TableCell>{row.username}</TableCell>
+                      <TableCell align="center">
+                        <IconButton
+                          onClick={() => handleEditRow(row)}
+                          size="small"
+                        >
+                          <EditIcon color="primary" />
+                        </IconButton>
+
+                        <DialogComponent
+                          component={
+                            <IconButton size="small">
+                              <DeleteIcon color="primary" />
+                            </IconButton>
+                          }
+                          dialogTitle={"¿Desea eliminar este elemento?"}
+                          dialogSubtitle={
+                            "Se eliminarán tambien sus tareas asignadas."
+                          }
+                          onConfirm={() => userDelete(row.id)}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={6} align="center">
+                      {"Ingrese usuarios"}
                     </TableCell>
                   </TableRow>
-                ))}
+                )}
               </TableBody>
             </Table>
           </TableContainer>
         </Grid>
       </Grid>
-
-      <br />
+      <br />{" "}
       <Grid container>
         <Grid item xs={12} component={Paper}>
-          <FormControl variant="filled" sx={{ m: 1, minWidth: 220 }}>
-            <InputLabel id="demo-simple-select-label">
-              Tipo de usuario
-            </InputLabel>
+          <Typography variant="h6" sx={{ margin: 1 }}>
+            {editingRowId !== null ? "Editar usuario" : "Agregar usuario"}
+          </Typography>{" "}
+          <Box>
+            <FormControl variant="filled" sx={{ m: 1, minWidth: 220 }}>
+              <InputLabel id="demo-simple-select-label">
+                Tipo de usuario
+              </InputLabel>
 
-            <Select
-              value={userData.usertype_id}
-              onChange={handleFormChange}
-              name="usertype_id"
-              displayEmpty
-            >
-              <MenuItem value="0" disabled>
-                Seleccione un rol
-              </MenuItem>
-              {tenantUserTypes.map((usertype) => (
-                <MenuItem key={usertype.id} value={usertype.id}>
-                  {usertype.name}
+              <Select
+                value={userData.usertype_id}
+                onChange={handleFormChange}
+                name="usertype_id"
+                displayEmpty
+              >
+                <MenuItem value="0" disabled>
+                  Seleccione un rol
                 </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <TextField
-            label="Apellido"
-            name="surname"
-            sx={{ m: 1 }}
-            value={userData.surname}
-            onChange={handleFormChange}
-          />
-          <TextField
-            label="Nombres"
-            name="names"
-            sx={{ m: 1 }}
-            value={userData.names}
-            onChange={handleFormChange}
-          />
-          <TextField
-            label="Email"
-            name="mail_address"
-            sx={{ m: 1 }}
-            value={userData.mail_address}
-            onChange={handleFormChange}
-            error={emailError}
-            helperText={emailError ? "El correo electrónico no es válido" : ""}
-            onKeyDown={(e) => {
-              if (e.key === " ") {
-                e.preventDefault();
+                {tenantUserTypes.map((usertype) => (
+                  <MenuItem key={usertype.id} value={usertype.id}>
+                    {usertype.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <br />
+            <TextField
+              label="Apellido"
+              name="surname"
+              sx={{ m: 1 }}
+              value={userData.surname}
+              onChange={handleFormChange}
+            />
+            <TextField
+              label="Nombres"
+              name="names"
+              sx={{ m: 1 }}
+              value={userData.names}
+              onChange={handleFormChange}
+            />
+            <br />
+            <TextField
+              label="Email"
+              name="mail_address"
+              sx={{ m: 1 }}
+              value={userData.mail_address}
+              onChange={handleFormChange}
+              error={emailError}
+              helperText={
+                emailError ? "El correo electrónico no es válido" : ""
               }
-            }}
-          />
-          <TextField
-            label="Nombre de usuario"
-            name="username"
-            sx={{ m: 1 }}
-            value={userData.username}
-            onChange={handleFormChange}
-            error={usernameError}
-            helperText={errorMessage}
-            onKeyDown={(e) => {
-              if (e.key === " ") {
-                e.preventDefault();
-              }
-            }}
-          />
-          <Button
-            type="submit"
-            onClick={handleValidation}
-            disabled={disableSubmit()} // Habilita o deshabilita el botón según el resultado de la función
-          >
-            {editingRowId !== null ? "Actualizar" : "Agregar"}
-          </Button>
+              onKeyDown={(e) => {
+                if (e.key === " ") {
+                  e.preventDefault();
+                }
+              }}
+            />
+            <TextField
+              label="Nombre de usuario"
+              name="username"
+              sx={{ m: 1 }}
+              value={userData.username}
+              onChange={handleFormChange}
+              error={usernameError}
+              helperText={errorMessage}
+              onKeyDown={(e) => {
+                if (e.key === " ") {
+                  e.preventDefault();
+                }
+              }}
+            />
+            <Box
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Button
+                type="submit"
+                color="error"
+                sx={{ margin: 1 }}
+                onClick={clearAllFields}
+                disabled={disableClean()}
+              >
+                {editingRowId !== null ? "Cancelar" : "Limpiar"}
+              </Button>
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ margin: 1 }}
+                onClick={handleValidation}
+                disabled={disableSubmit()}
+              >
+                {editingRowId !== null ? "Actualizar" : "Agregar"}
+              </Button>
+            </Box>
+          </Box>
         </Grid>
       </Grid>
+      <br />
       <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
         <Button sx={{ mt: 3, ml: 1 }} onClick={onBack}>
           Regresar
